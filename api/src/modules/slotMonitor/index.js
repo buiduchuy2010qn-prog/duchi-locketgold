c16661bd6f1cbbd3477e7abbc711486db848a74e
buiduchuy2010qn-prog <buiduchuy2010qn@gmail.com>
2026-08-07 23:19:53 +0700

    feat(telegram): khởi động bot helper cùng Canh Slot

--- api/src/modules/slotMonitor/index.js
+++ api/src/modules/slotMonitor/index.js
@@ -1,5 +1,12 @@
 const slotMonitorRoutes = require("./routes");
-const { startSlotMonitorWorker } = require("./service");
+const { startSlotMonitorWorker: startCoreSlotMonitorWorker } = require("./service");
+const { startTelegramBotPolling } = require("./telegramBot");
+
+function startSlotMonitorWorker() {
+  const workerStarted = startCoreSlotMonitorWorker();
+  startTelegramBotPolling();
+  return workerStarted;
+}
 
 module.exports = {
   slotMonitorRoutes,
